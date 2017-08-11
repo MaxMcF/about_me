b349c800d03473a7524409bb420d9457dc307d66
MAX MCFARLAND <mmcfarland522@gmail.com>
2017-08-11 13:59:57 -0700

    Fixed console log, added name, displayed correctness after each question

--- app.js
+++ app.js
@@ -1,35 +1,76 @@
 'use strict';
 
-alert('ITS TIME TO PLAY..... THREE TRUTHS AND TWO LIES! (yes or no answers only) There are only two wrong answers, so guess wisely!');
+var name = prompt('ITS TIME TO PLAY..... THREE TRUTHS AND TWO LIES! (yes or no answers only) There are only two wrong answers, so guess wisely! BUT FIRST: What is your name?').toUpperCase();
+
 var q2 = 'My great-grandfather was one of the attorneys that put Al Capone in jail.';
+console.log(q2);
 var answer = prompt(q2);
+console.log(answer);
 var res = answer.toUpperCase();
+if (res === 'YES' || res === 'Y') {
+  alert('You got that one correct! Nice!');
+}else if (res === 'NO' || res === 'N'){
+  alert('INCORRECT');
+}else{
+  alert('Yes or No answers only!');
+}
 var answerlog = [];
-console.log(res);
 answerlog.push(res);
 var q2 = 'I\'ve eaten a snake and a guinea pig before.';
+console.log(q2);
 answer = prompt(q2);
+console.log(answer);
 res = answer.toUpperCase();
-console.log(res);
+if (res === 'YES' || res === 'Y') {
+  alert('You got that one correct! Nice!');
+}else if (res === 'NO' || res === 'N'){
+  alert('INCORRECT');
+}else{
+  alert('Yes or No answers only!');
+}
 answerlog.push(res);
 var q2 = 'I was once nearly the victim of a major american terrorist attack.';
+console.log(q2);
 answer = prompt(q2);
+console.log(answer);
 res = answer.toUpperCase();
-console.log(res);
+if (res === 'YES' || res === 'Y') {
+  alert('You got that one correct! Nice!');
+}else if (res === 'NO' || res === 'N'){
+  alert('INCORRECT');
+}else{
+  alert('Yes or No answers only!');
+}
 answerlog.push(res);
 var q2 = 'I once sat next to Martin Sheen on a plane and threw up on him after having one too many drinks...';
+console.log(q2);
 answer = prompt(q2);
+console.log(answer);
 res = answer.toUpperCase();
-console.log(res);
+if (res === 'NO' || res === 'N') {
+  alert('You got that one correct! Nice!');
+}else if (res === 'YES' || res === 'Y'){
+  alert('INCORRECT');
+}else{
+  alert('Yes or No answers only!');
+}
 answerlog.push(res);
 var q2 = 'I play lead gutiar in a Jimi Hendrix cover band.';
+console.log(q2);
 answer = prompt(q2);
+console.log(answer);
 res = answer.toUpperCase();
-console.log(res);
+if (res === 'NO' || res === 'N') {
+  alert('You got that one correct! Nice!');
+}else if (res === 'YES' || res === 'Y'){
+  alert('INCORRECT');
+}else{
+  alert('Yes or No answers only!');
+}
 answerlog.push(res);
 if((answerlog[0] === 'YES' || answerlog[0] === 'Y' ) && (answerlog[1] === 'YES' || answerlog[1] === 'Y' ) && ( answerlog[2] === 'YES' || answerlog[2] === 'Y') && (answerlog[3] === 'NO' || answerlog[3] === 'N' ) && (answerlog[4] === 'NO' || answerlog[4] === 'N')){
-  alert('YOU ARE CORRECT!!! Congradulations! Give yourself a nice long pat on the back');
+  alert('YOU GOT ALL OF THE ANSWERS CORRECT ' + name + '!!! Congratulations! Give yourself a nice long pat on the back');
 }else{
-  alert('INCORRECT... TRY AGAIN');
+  alert('FAILURE!!!!! YOU DIDN\'T GET THEM ALL CORRECT ' + name + '!... Better luck next time');
 }
 console.log(answerlog);
